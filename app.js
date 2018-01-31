1804bf570ba2f0b28b2ca2025f70a1aed7feb412
McConchie <31684285+McConchie@users.noreply.github.com>
2018-01-31 19:14:11 +0200

    Update

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var about = require('./routes/Ari');
+var about = require('./routes/ari');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/Ari', Ari);
+app.use('/ari', ari);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
