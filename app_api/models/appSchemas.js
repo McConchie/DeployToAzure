8fcdfb157bd4d340be0bfb0ce1b95f0676d3ee00
McConchie <31684285+McConchie@users.noreply.github.com>
2018-04-12 09:52:47 +0300

    Add files via upload

--- app_api/models/appSchemas.js
+++ app_api/models/appSchemas.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const firstSchema = new mongoose.Schema ({name:String, ranking:String});
 const secondSchema = new mongoose.Schema ({name:String, ranking:String});
 
-mongoose.model('2016', firstSchema, 'firstYear');
-mongoose.model('2017', secondSchema, 'secondYear');
+mongoose.model('first', firstSchema, 'firstYear');
+mongoose.model('second', secondSchema, 'secondYear');
