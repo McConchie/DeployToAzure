b0adbec1ca3f29334dddfe27346d3fe93c50823e
McConchie <31684285+McConchie@users.noreply.github.com>
2018-04-12 10:16:04 +0300

    Update appSchemas.js

--- app_api/models/appSchemas.js
+++ app_api/models/appSchemas.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose');
 const firstSchema = new mongoose.Schema ({name:String, ranking:String});
 const secondSchema = new mongoose.Schema ({name:String, ranking:String});
 
-mongoose.model('first', firstSchema, 'firstYear');
-mongoose.model('second', secondSchema, 'secondYear');
+mongoose.model('firstYear', firstSchema, 'firstYear');
+mongoose.model('secondYear', secondSchema, 'secondYear');
